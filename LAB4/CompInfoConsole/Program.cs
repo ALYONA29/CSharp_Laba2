71d8179b216e79e6396199a264c80eb1f6203a8d
ALYONA29 <62483180+ALYONA29@users.noreply.github.com>
2020-04-09 21:11:34 +0300

    Add files via upload

---
+++ LAB4/CompInfoConsole/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Runtime.InteropServices; 
+using System.Management;   
+
+namespace CompInfoConsole
+{
+    class Program
+    {
+        [DllImport("kernel32.dll", CallingConvention = CallingConvention.Winapi)]
+        static extern bool GetPhysicallyInstalledSystemMemory(ref ulong memoryLength);
+
+        [DllImport("kernel32.dll", CallingConvention = CallingConvention.Winapi)]
+        static extern bool GetComputerNameEx(COMPUTER_NAME_FORMAT NameType,  StringBuilder lpBuffer, ref uint lpnSize);
+
+        enum COMPUTER_NAME_FORMAT
+        {
+            ComputerNameNetBIOS,
+            ComputerNameDnsHostname,
+            ComputerNameDnsDomain,
+            ComputerNameDnsFullyQualified,
+            ComputerNamePhysicalNetBIOS,
+            ComputerNamePhysicalDnsHostname,
+            ComputerNamePhysicalDnsDomain,
+            ComputerNamePhysicalDnsFullyQualified
+        }
+
+        static void Main(string[] args)
+        {
+            
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
+            foreach (ManagementObject wmi_CPU in searcher.Get())
+            {
+                Console.WriteLine("Процессор: {0}", wmi_CPU["Name"]);
+            }
+
+            ulong memoryLength = 0;
+            bool rc = true;
+
+            rc = GetPhysicallyInstalledSystemMemory(ref memoryLength);
+            Console.WriteLine("Установленная память: {0} Гб", memoryLength / 1024 / 1024.0);
+            
+            StringBuilder name = new StringBuilder(260);
+            uint size = 260;
+            rc = GetComputerNameEx(COMPUTER_NAME_FORMAT.ComputerNameDnsFullyQualified, name, ref size);
+            Console.WriteLine("Компьютер: {0}", name);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LAB4/fibonacci/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Runtime.InteropServices;
+
+namespace fibonacci
+{
+    class Program
+    {
+        [DllImport("LibCPP.dll", EntryPoint = "fibonacciCDECL", CallingConvention = CallingConvention.Cdecl)]
+        static extern UInt32 fibonacciCDECL(UInt32 m);
+
+        [DllImport("LibCPP.dll", EntryPoint = "_fibonacciSTD@4", CallingConvention = CallingConvention.StdCall)]
+        static extern UInt32 fibonacciSTD(UInt32 m);
+
+        [DllImport("LibCPP.dll", EntryPoint = "@fibonacciFAST@4", CallingConvention = CallingConvention.FastCall)]
+        static extern UInt32 fibonacciFAST(UInt32 m);
+
+
+        [DllImport("LibCPP.dll", EntryPoint = "createFibonacci", CallingConvention = CallingConvention.Cdecl)]
+        static extern IntPtr createFibonacci(UInt32 m);
+
+        [DllImport("LibCPP.dll", EntryPoint = "?calc@fibonacciNumber@@QAEKXZ", CallingConvention = CallingConvention.ThisCall)]
+        static extern UInt32 сalc(IntPtr instance);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер числа Фибоначчи, которое вы хотите найти");
+            UInt32 m;
+            string input = Console.ReadLine();
+            while (!(UInt32.TryParse(input, out m)) || m <= 0)
+            {
+                Console.WriteLine("Неверный ввод, попробуйте снова : ");
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Cdecl.   fib({0}) = {1}", m, fibonacciCDECL(m));
+
+            Console.WriteLine("StdCall. fib({0}) = {1}", m, fibonacciSTD(m));
+
+            IntPtr fibClass = createFibonacci(m);
+            Console.WriteLine("ThisCall.   fib({0}) = {1}", m, сalc(fibClass));
+
+            Console.ReadKey();
+        }
+    }
+}
